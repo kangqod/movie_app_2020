72b353d1c808e6b28626c92f719941353af6e9a6
kGokUHoK3S <kangqod@naver.com>
2020-11-08 15:51:54 +0900

    Food add method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Potato from './Potato'
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>
+}
 
 function App() {
   return (
     <>
       <h1>Hello!!!!!</h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </>
   );
 }
