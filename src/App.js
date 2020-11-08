3a233b8091564e7c289c5430199571193da783b3
kGokUHoK3S <kangqod@naver.com>
2020-11-08 16:39:33 +0900

    Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import axios from 'axios'
+
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 1000)
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json')
+    console.log(movies)
   }
+
+  componentDidMount() {
+    this.getMovies()
+  }
+
   render() {
     const { isLoading } = this.state
     return <div>{isLoading ? 'Loading...' : "We are ready"}</div>
